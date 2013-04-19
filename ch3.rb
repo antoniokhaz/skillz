f71fadf3e4bd5fa3472096cd00da29f5c2363e7e
Antonio Khazdozian <antoniok@amcoonline.net>
2013-04-19 11:37:58 -0700

    adds up to chapter 5

---
+++ ch3.rb
@@ -0,0 +1,24 @@
+puts 'Hello.'
+puts 'Ruby rocks.'
+puts 'Nobody deserves a mime, Buffy.'
+puts 'Snoppy says #%^?&*@! when he stubs his toe.'
+puts '		'
+puts ''
+puts 'I like' + 'apple pie.'
+puts 'I like ' + 'apple pie'
+puts 'I like' + ' apple pie'
+
+puts 'blink ' * 4
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+puts 'You\'re swell'
+puts 'backslash at the end of a string:  \\'
+puts 'up\\down'
+puts 'up\down'
