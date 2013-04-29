01c5c8ee30e6d55c9300dd4dad87c4abfb975414
Antonio Khazdozian <antoniok@amcoonline.net>
2013-04-29 15:21:39 -0700

    Beer song exercise

---
+++ l2p/ch7_beerSong.rb
@@ -0,0 +1,28 @@
+wallBeers = 5
+newBeers = wallBeers
+
+while newBeers > 2
+
+	puts newBeers.to_s + ' Bottles of Beer on the Wall.'
+	puts newBeers.to_s + ' Bottles of Beer'
+	puts 'You take one down and pass it around'
+	newBeers = newBeers - 1
+	puts newBeers.to_s + ' Bottles of Beer on the Wall'
+	puts ''
+	
+end
+
+while newBeers <= 2
+	puts newBeers.to_s + ' Bottles of Beer on the wall'
+	puts newBeers.to_s + ' Bottles of Beer'
+	puts 'You take one down and pass it around'
+	newBeers = newBeers - 1
+	puts newBeers.to_s + ' Bottle of Beer on the wall'
+	puts ''
+	puts newBeers.to_s + ' Bottle of Beer on the wall'
+	puts newBeers.to_s + ' Bottle of Beer'
+	puts 'You take one down and pass it around'
+	newBeers = newBeers - 1
+	puts newBeers.to_s + ' Bottles of Beer on the wall'
+	break
+end
