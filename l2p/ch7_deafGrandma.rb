278190d4e451281da266b621504c24b80071924a
Antonio Khazdozian <antoniok@amcoonline.net>
2013-05-01 20:02:57 -0700

    updated Deaf Grandma exercise

--- l2p/ch7_deafGrandma.rb
+++ l2p/ch7_deafGrandma.rb
@@ -1,17 +1,28 @@
 puts '... Hellouuuu dear.'
 
+goodBye = 0
 
 while true
 	input = gets.chomp
 	if input == 'BYE'
-		puts 'SEE YOU LATER DEAR!'
-		break
+		goodBye = goodBye + 1
+		puts 'WHAT?!'
+		puts ''
+		if goodBye >= 3
+			puts 'SEE YOU LATER DEAR!'
+			break
+		end
+	else
+		goodBye = 0
 	end
+	
 		
 	if input != input.upcase
 		puts 'HUH SPEAK UP, SONNY!'
-	else
+		puts ''
+	elsif input != 'BYE'
 		year = 1930 + rand(21)
 		puts 'NO, NOT SINCE ' + year.to_s + '!'
+		puts ''
 	end
 end
