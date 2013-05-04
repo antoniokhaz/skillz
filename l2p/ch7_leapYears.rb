9698e0749de4621133d9a6321a26b736a43b2046
Antonio Khazdozian <antoniok@amcoonline.net>
2013-05-03 18:41:53 -0700

    Pushed Leap Year exercise.

---
+++ l2p/ch7_leapYears.rb
@@ -0,0 +1,26 @@
+
+puts 'Hey would you like to know some leap years?'
+puts 'Just give me a starting year'
+starting = gets.chomp
+puts ''
+
+puts 'And now an ending year'
+ending = gets.chomp
+puts ''
+
+yearsBetween = ending.to_i
+
+puts 'Here are the leap years between ' + starting.to_s + ' and ' + ending.to_s + ':'
+puts ''
+
+while yearsBetween.to_i >= starting.to_i
+	yearsBetween = yearsBetween - 1  
+
+	if yearsBetween%4 == 0
+		
+		leapYear = yearsBetween.to_i
+		puts '- ' + leapYear.to_s + ' is a leap year'
+	end
+end
+puts ''
+puts 'Hope this helped, see ya!'
