2646d1c93c90ba15db8ce9da4b9d6775ec6ae3f5
Antonio Khazdozian <antoniok@amcoonline.net>
2013-05-06 04:48:50 -0700

    Table of Contents, Revisited w/ arrays

---
+++ l2p/ch8_table_of_contents.rb
@@ -0,0 +1,19 @@
+line_width = 60
+
+header = 'Table of Contents'
+puts header.center(line_width)
+
+chapters = [['1: Getting Started', ' 1'],
+            ['2: Numbers',  ' 9'], 
+            ['3: Letters', '13']]
+
+chapters.each do |ch|
+	chap = ch[0]
+	page = ch[1]
+
+	left =  'Chapter ' + chap
+	right = 'page ' + page
+
+	puts left.ljust(line_width/2) + right.rjust(line_width/2)
+end
+
