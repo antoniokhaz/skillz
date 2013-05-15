cff26aee93c6a02516824349508214dfea98657c
Antonio Khazdozian <antoniok@amcoonline.net>
2013-05-15 13:03:13 -0700

    Leap Years exercise... finally correct?

--- l2p/ch7_leapYears.rb
+++ l2p/ch7_leapYears.rb
@@ -13,13 +13,12 @@ puts 'Here are the leap years between ' + starting.to_s + ' and ' + ending.to_s 
 puts ''
 
 while yearsBetween.to_i >= starting.to_i
-	yearsBetween = yearsBetween - 1  
-
-	if yearsBetween%4 == 0 || yearsBetween%400 == 0 && yearsBetween%100 != 0
-		
-		leapYear = yearsBetween.to_i
-		puts '- ' + leapYear.to_s + ' is a leap year'
+	
+	if yearsBetween%4 == 0 && (yearsBetween%400 == 0 || yearsBetween%100 != 0)
+			leapYear = yearsBetween.to_i
+			puts '- ' + leapYear.to_s + ' is a leap year'
 	end
+	yearsBetween = yearsBetween - 1  
 end
 puts ''
 puts 'Hope this helped, see ya!'
